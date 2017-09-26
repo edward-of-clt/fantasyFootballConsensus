b786da17d8bb24b4d3ff2e76da32cd584543b3c5
eli-townsend <info@established1992.com>
2017-09-26 15:19:31 -0400

    drag and drop

--- js/dice.js
+++ js/dice.js
@@ -1,3 +1,55 @@
+/****
+32 Team array
+**********/
+var NFLTeams = ["Arizona","Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas-City", "LA-Rams", "LA-Chargers", "Miami", "Minnesota", "New-England", "New-Orleans", "NY-Jets", "NY-Giants", "Oakland", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa-Bay", "Tennessee", "Washington"];
+
+/***
+Rosters Array
+******/
+  var rosters = ['teamTy', 'teamTravis', 'teamBryant', 'teamElijah', 'teamBrad', 'teamKiran', 'teamMiller', 'teamKnox', 'teamDaniel', 'teamMatt'];
+
+/***
+Drag and drop functionality
+******/
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+/***
+Select Keepers and remove them from NFLTeams array
+******/
+
+function remove(arr, what) {
+  var found = arr.indexOf(what);
+
+  while (found !== -1) {
+      arr.splice(found, 1);
+      found = arr.indexOf(what);
+  }
+}
+
+  function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+      console.log(data);
+
+    for (var i = 0; i < NFLTeams.length; i++)
+      if (NFLTeams[i] === data) {
+          NFLTeams.splice(i, 1);
+          break;
+      }
+      console.log(NFLTeams);
+};
+
+
+/****
+Shuffle NFLTeams array
+*******/
 function shuffle(arra1) {
     var ctr = arra1.length, temp, index;
     while (ctr > 0) {
@@ -10,14 +62,13 @@ function shuffle(arra1) {
     return arra1;
 }
 
-class Player{
-  var teams = [];
-  var name;
-}
+/****
+Loop through teams  === 3, and print randomized teams to rosterTeam
+*******/
 
-var NFLTeams = ["Arizona","Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas City", "LA Rams", "LA Chargers", "Miami", "Minnesota", "New England", "New Orleans", "NY Jets", "NY Giants", "Oakland", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa Bay", "Tennessee", "Washington"];
-
-
-function diceRoll(){
-document.getElementById("teams").innerHTML = shuffle(NFLTeams);
-}
+for (var index = 0; index < NFLTeams.length; index++)
+    {
+        NFLTeams[index] = (NFLTeams[index]);
+        document.getElementById(rosters[index]).innerHTML = NFLTeams[index];
+        console.log(NFLTeams[index]);
+    }
