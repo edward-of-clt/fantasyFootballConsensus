fccf2d85391c2425668991e86011080b326fd36b
eli-townsend <info@established1992.com>
2017-09-29 10:22:19 -0400

    javascript object work

--- js/ooj.js
+++ js/ooj.js
@@ -3,10 +3,66 @@
 **********/
 var NFLTeams = ["Arizona","Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas-City", "LA-Rams", "LA-Chargers", "Miami", "Minnesota", "New-England", "New-Orleans", "NY-Jets", "NY-Giants", "Oakland", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa-Bay", "Tennessee", "Washington"];
 
+var fullTeams = {
+  Team1: {
+      Value: 'arizona',
+      DisplayName: "Arizona"
+    },
+    Team2: {
+      Value: 'atlanta',
+      DisplayName: "Atlanta"
+    },
+    Team3: {
+      Value: 'baltimore',
+      DisplayName: "Baltimore"
+    },
+    Team4: {
+      Value: 'buffalo',
+      DisplayName: "Buffalo"
+    },
+    Team5: {
+      Value: 'carolina',
+      DisplayName: "Carolina"
+    },
+    Team6: {
+      Value: 'chicago',
+      DisplayName: "Chicago"
+    },
+    Team7: {
+      Value: 'cincinnati',
+      DisplayName: "Cincinnati"
+    },
+    Team8: {
+      Value: 'cleveland',
+      DisplayName: "Cleveland"
+    }
+};
+
+for (var key in fullTeams) {
+      if (fullTeams.hasOwnProperty(key)) {
+        console.log(fullTeams[0].DisplayName);
+      }
+    }
+
 /***
 Rosters Array
 ******/
   var rosters = ['teamTy', 'teamTravis', 'teamBryant', 'teamElijah', 'teamBrad', 'teamKiran', 'teamMiller', 'teamKnox', 'teamDaniel', 'teamMatt'];
+
+  /****
+  Shuffle/Randomize Array
+  ******/
+function shuffle(arra1) {
+      var ctr = arra1.length, temp, index;
+      while (ctr > 0) {
+          index = Math.floor(Math.random() * ctr);
+          ctr--;
+          temp = arra1[ctr];
+          arra1[ctr] = arra1[index];
+          arra1[index] = temp;
+      }
+      return arra1;
+  }
 
 
 function weeklyTeam(name, teams, keeper){
@@ -18,59 +74,10 @@ function weeklyTeam(name, teams, keeper){
   };
   this.keeper = false;
   this.display = function(){
-    document.getElementById(this.name).innerHTML = shuffle(NFLTeams);
+    alert(this.teams);
   };
 }
 
-/***
-Drag and drop functionality
-******/
-function allowDrop(ev) {
-    ev.preventDefault();
-}
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-/***
-Select Keepers and remove them from NFLTeams array
-******/
-
-function remove(arr, what) {
-  var found = arr.indexOf(what);
-
-  while (found !== -1) {
-      arr.splice(found, 1);
-      found = arr.indexOf(what);
-  }
-}
-
-  function drop(ev) {
-      ev.preventDefault();
-      var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(data));
-      console.log(data);
-
-    for (var i = 0; i < NFLTeams.length; i++)
-      if (NFLTeams[i] === data) {
-          NFLTeams.splice(i, 1);
-          break;
-      }
-      console.log(NFLTeams);
-};
-
-/****
-Shuffle NFLTeams array
-*******/
-function shuffle(arra1) {
-    var ctr = arra1.length, temp, index;
-    while (ctr > 0) {
-        index = Math.floor(Math.random() * ctr);
-        ctr--;
-        temp = arra1[ctr];
-        arra1[ctr] = arra1[index];
-        arra1[index] = temp;
-    }
-    return arra1;
-}
+shuffle(NFLTeams);
+console.log(NFLTeams);
