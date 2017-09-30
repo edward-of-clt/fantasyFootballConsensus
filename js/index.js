958f083847873e884f901b817839ab916b2df25b
Edward Herbert <eherbert@bgea.org>
2017-09-29 20:23:57 -0400

    We love Objects

--- js/index.js
+++ js/index.js
@@ -1,65 +1,95 @@
-function shuffle(arra1) {
-    var ctr = arra1.length, temp, index;
-    while (ctr > 0) {
-        index = Math.floor(Math.random() * ctr);
-        ctr--;
-        temp = arra1[ctr];
-        arra1[ctr] = arra1[index];
-        arra1[index] = temp;
-    }
-    return arra1;
-}
-
 var NFLTeams = ["Arizona","Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas City", "LA Rams", "LA Chargers", "Miami", "Minnesota", "New England", "New Orleans", "NY Jets", "NY Giants", "Oakland", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa Bay", "Tennessee", "Washington"];
 
+var Teams = [ "Ty Eades", "Travis Hyatt", "John Bryant", "Elijah Townsend", "Brad Royal", "Kiran Mangipudi", "John Miller", "Ryan Knox", "Daniel Hedrick", "Matt Armfield" ];
 
-function diceRoll(){
-document.getElementById("teams").innerHTML = shuffle(NFLTeams);
 
-function remove(arr, what) {
-  var found = arr.indexOf(what);
+var NFLApp = class {
 
-  while (found !== -1) {
-      arr.splice(found, 1);
-      found = arr.indexOf(what);
+  constructor(Teams,NFLTeams) {
+    this.teams = Teams;
+    this.NFLTeams = NFLTeams;
+
+    this.info = {
+        count: this.teams.length-2,
+        current: -1
+    };
+
+    this.showTeams();
+    this.showKeepers();
+
+    this.documentready();
   }
-}
 
+  documentready() {
+    document.getElementById('roll-button').setAttribute('style','');
+  }
 
-var checkedValue = null;
-var inputElements = document.getElementsByClassName('teamBox');
-for(var i=0; inputElements[i]; ++i){
-      if(inputElements[i].checked){
-        checkedValue = inputElements[i].value;
-        remove(NFLTeams, checkedValue);
-        continue;
-      } else {
-        break;
+  showTeams() {
+    document.getElementById('draftTeam').innerHTML = '<h1>Teams</h1>';
+    for(var x in this.teams) {
+      document.getElementById('draftTeam').innerHTML = this.getContent('draftTeam')+'<li>'+this.teams[x]+'<div id="team'+x+'"></div></li>';
+    }
+  }
+
+  getContent(id) {
+    return document.getElementById(id).innerHTML;
+  }
+
+  showKeepers() {
+    document.getElementById('keepers').innerHTML = '<h1>Keepers</h1>';
+    for(var x in this.NFLTeams) {
+      document.getElementById('keepers').innerHTML = this.getContent('keepers')+' <label>'+this.NFLTeams[x]+' <input value="'+this.NFLTeams[x]+'" id="NFLTeam'+x+'" class="teamBox" type="checkbox"></label><br/>';
+    }
+  }
+
+  shuffleNFL() {
+      var ctr = this.NFLTeams.length, temp, index;
+      while (ctr > 0) {
+          index = Math.floor(Math.random() * ctr);
+          ctr--;
+          temp = this.NFLTeams[ctr];
+          this.NFLTeams[ctr] = this.NFLTeams[index];
+          this.NFLTeams[index] = temp;
       }
+      return this.NFLTeams;
+  }
+
+  removeFromList(team) {
+      var found = this.NFLTeams.indexOf(team);
+
+      while (found !== -1) {
+          this.NFLTeams.splice(found, 1);
+          found = this.NFLTeams.indexOf(team);
+      }
+  }
+
+  addToOwner(team,id) {
+
+    if(this.info.count < this.info.current) {
+      this.info.current = 0;
+    } else {
+      this.info.current++;
+    }
+
+    document.getElementById('team'+this.info.current).innerHTML = this.getContent('team'+this.info.current)+'<div>'+team+'</div>';
+  }
+
+  diceRoll(){
+      document.getElementById("teams").innerHTML = this.shuffleNFL();
+
+      var checkedValue = null;
+
+      if(this.NFLTeams.length-2 > this.teams.length) {
+        for(var i=0; i < this.teams.length; ++i){
+              if(!document.getElementById('NFLTeam'+i).checked){
+                checkedValue = this.NFLTeams[i];
+                this.removeFromList(checkedValue);
+                this.addToOwner(checkedValue);
+              }
+        }
+      }
+  }
+
 }
 
-var indexToSplit = NFLTeams.indexOf('c');
-var first = NFLTeams.slice(0, 3);
-var second = NFLTeams.slice(3, 6);
-var third = NFLTeams.slice(6, 9);
-var fourth = NFLTeams.slice(9, 12);
-var fifth = NFLTeams.slice(12, 15);
-var sixth = NFLTeams.slice(15, 18);
-var seventh = NFLTeams.slice(18, 21);
-var eighth = NFLTeams.slice(21, 24);
-var nineth = NFLTeams.slice(24, 27);
-var tenth = NFLTeams.slice(27, 30);
-var teamsLeft = NFLTeams.slice(30, 32);
-
-document.getElementById("team1").innerHTML = first;
-document.getElementById("team2").innerHTML = second;
-document.getElementById("team3").innerHTML = third;
-document.getElementById("team4").innerHTML = fourth;
-document.getElementById("team5").innerHTML = fifth;
-document.getElementById("team6").innerHTML = sixth;
-document.getElementById("team7").innerHTML = seventh;
-document.getElementById("team8").innerHTML = eighth;
-document.getElementById("team9").innerHTML = nineth;
-document.getElementById("team10").innerHTML = tenth;
-document.getElementById("team11").innerHTML = teamsLeft;
-}
+var app = new NFLApp(Teams,NFLTeams);
